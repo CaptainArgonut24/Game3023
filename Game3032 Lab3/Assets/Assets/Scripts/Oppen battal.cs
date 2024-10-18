9305f06530cca33c52c90505f06533b40af0c93e
John <johnhusky400@gmail.com>
2024-10-18 19:44:30 -0400

    Lab done

--- Game3032 Lab3/Assets/Assets/Scripts/Oppen battal.cs
+++ Game3032 Lab3/Assets/Assets/Scripts/Oppen battal.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class Oppenbattal : MonoBehaviour
+public class TilemapTriggerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject uiElement; // Assign the UI element in the Inspector
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Check if the object colliding with the tilemap has the "Player" tag
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Enable the UI element when the player collides with the tilemap
+            uiElement.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        // Disable the UI element when the player exits the collision
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            uiElement.SetActive(false);
+        }
     }
 }
